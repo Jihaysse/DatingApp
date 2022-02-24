2c2421dad0d7b34937bbe8a405520f6606993f02
Jihaysse <js.segers@hotmail.com>
2022-02-24 17:07:53 +0100

    Added Register feature and home view

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,34 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {};
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    this.accountService.register(this.model).subscribe(
+      {
+        next: (response) => {
+          this.cancel(); // cancel register to go back to home view
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+  }
+
+  cancel() {
+    this.cancelRegister.emit()
+  }
+
+}
